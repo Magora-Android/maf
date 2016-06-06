31ab4865baeea49bec263c93ef9704ae4540d589
Valentin S. Karpenko <karpenko@magora.systems>
2016-06-06 15:07:22 +0600

    First commit
    Created samples project

---
+++ samples/src/main/java/com/magorasystems/mafmodules/ui/activity/SampleActivity.java
@@ -0,0 +1,18 @@
+package com.magorasystems.mafmodules.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Developed by Magora Team (magora-systems.com). 2016.
+ *
+ * @author Valentin S.Bolkonsky
+ */
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
