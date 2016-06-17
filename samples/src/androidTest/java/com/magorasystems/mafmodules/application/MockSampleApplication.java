bd19d645b76da8fa11e9d55129ffb5d07927e4d8
Valentin S. Karpenko <karpenko@magora.systems>
2016-06-17 12:24:19 +0600

    Add mock application
    Created custom instrumental test runner.

---
+++ samples/src/androidTest/java/com/magorasystems/mafmodules/application/MockSampleApplication.java
@@ -0,0 +1,27 @@
+package com.magorasystems.mafmodules.application;
+
+import com.magorasystems.mafmodules.dagger.DaggerTestInner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Developed by Magora Team (magora-systems.com). 2016.
+ *
+ * @author Valentin S.Bolkonsky
+ */
+public class MockSampleApplication extends SampleApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MockSampleApplication.class);
+
+    @Override
+    public void onCreate() {
+        LOGGER.info("Mock application is running...");
+        super.onCreate();
+    }
+
+    @Override
+    protected void buildGraphAndInject() {
+        setComponent(DaggerTestInner.buildGraph(this));
+    }
+}
---
+++ samples/src/androidTest/java/com/magorasystems/mafmodules/runner/MockAndroidJUnitRunner.java
@@ -0,0 +1,20 @@
+package com.magorasystems.mafmodules.runner;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.test.runner.AndroidJUnitRunner;
+
+import com.magorasystems.mafmodules.application.MockSampleApplication;
+
+/**
+ * Developed by Magora Team (magora-systems.com). 2016.
+ *
+ * @author Valentin S.Bolkonsky
+ */
+public class MockAndroidJUnitRunner extends AndroidJUnitRunner {
+
+    @Override
+    public Application newApplication(ClassLoader cl, String className, Context context) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        return super.newApplication(cl, MockSampleApplication.class.getName(), context);
+    }
+}
