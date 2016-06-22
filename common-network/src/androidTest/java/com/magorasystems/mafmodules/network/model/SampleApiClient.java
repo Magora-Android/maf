e8a0d2019d9212792e3df7142518487976158c81
Valentin S. Karpenko <karpenko@magora.systems>
2016-06-22 19:12:01 +0600

    Remove unused test files

--- common-network/src/androidTest/java/com/magorasystems/mafmodules/network/model/SampleApiClient.java
+++
@@ -1,9 +0,0 @@
-package com.magorasystems.mafmodules.network.model;
-
-/**
- * Developed by Magora Team (magora-systems.com). 2016.
- *
- * @author Valentin S.Bolkonsky
- */
-public interface SampleApiClient {
-}
