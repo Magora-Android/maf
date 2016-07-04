243b2ad1c0074bd06a61932ebc0e0186cb1a6894
magora_vlasov <vlasov@magora-systems.com>
2016-07-04 16:12:39 +0600

    Add doc for ViewInput interface

--- common-module/src/main/java/com/magorasystems/mafmodules/common/module/input/ViewInput.java
+++ common-module/src/main/java/com/magorasystems/mafmodules/common/module/input/ViewInput.java
@@ -3,11 +3,12 @@ package com.magorasystems.mafmodules.common.module.input;
 import com.magorasystems.mafmodules.common.mvp.view.BaseView;
 
 /**
- * Developed by Magora Team (magora-systems.com). 2016.
- *
+ * @param <P> param for {@code passive view} that must be extends {@link BaseView}
+ * @param <I> param for {@code interactive view} that must be extends {@link InteractiveView}
  * @author Valentin S.Bolkonsky
+ *         Developed by Magora Team (magora-systems.com). 2016.
  */
-public interface ViewInput<P extends BaseView,I extends InteractiveView<?>> {
+public interface ViewInput<P extends BaseView, I extends InteractiveView<?>> {
 
     P getPassiveView();
 
