842486ade7cf8546076d763ddabc1f1fe82defb0
magora_vlasov <vlasov@magora-systems.com>
2016-07-04 16:08:45 +0600

    Add doc for AbstractViewOutput

--- common-module/src/main/java/com/magorasystems/mafmodules/common/module/output/AbstractViewOutput.java
+++ common-module/src/main/java/com/magorasystems/mafmodules/common/module/output/AbstractViewOutput.java
@@ -1,18 +1,26 @@
 package com.magorasystems.mafmodules.common.module.output;
 
 /**
- * Developed by Magora Team (magora-systems.com). 2016.
+ * @param <M> type of your output model
  *
  * @author Valentin S.Bolkonsky
+ *         Developed by Magora Team (magora-systems.com). 2016.
  */
 public abstract class AbstractViewOutput<M> implements ViewOutput<M> {
 
     private M model;
 
+    /**
+     * Setting model
+     * @param model your model
+     */
     protected AbstractViewOutput(M model) {
         this.model = model;
     }
 
+    /**
+     * @return model
+     */
     @Override
     public M getModel() {
         return model;
