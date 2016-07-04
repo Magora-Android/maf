31405549681d9506b7f121321583925067233401
magora_vlasov <vlasov@magora-systems.com>
2016-07-04 12:15:36 +0600

    Add doc for ParserUtils

--- common-utils/src/main/java/com/magorasystems/mafmodules/common/utils/ParserUtils.java
+++ common-utils/src/main/java/com/magorasystems/mafmodules/common/utils/ParserUtils.java
@@ -15,6 +15,15 @@ public final class ParserUtils {
 
     }
 
+    /**
+     * Parsing String to BigDecimal
+     *
+     * @param src a String containing the BigDecimal representation to be parsed
+     * @return <b>null</b> if {@code src} == null <br>
+     * <b>null</b> if throws NumberFormatException <br>
+     * <b>instance</b> of BigDecimal with your parameters, otherwise
+     * @throws NumberFormatException if {@code src} does not contain a valid string representation of a big decimal.
+     */
     public static BigDecimal parseToDecimal(String src) {
         if (src == null) {
             return null;
@@ -27,6 +36,15 @@ public final class ParserUtils {
         return null;
     }
 
+    /**
+     * Parsing String to Long
+     *
+     * @param src a string containing the long representation to be parsed
+     * @return <b>null</b> if {@code src} == null <br>
+     * <b>null</b> if throws NumberFormatException <br>
+     * <b>instance</b> of Long with your parameters, otherwise
+     * @throws NumberFormatException if {@code src} does not contain a valid string representation of a long.
+     */
     public static Long parseToLong(String src) {
         if (src == null) {
             return null;
@@ -39,6 +57,15 @@ public final class ParserUtils {
         return null;
     }
 
+    /**
+     * Parsing String to Double
+     *
+     * @param src the double value represented by the string argument.
+     * @return <b>null</b> if {@code src} == null <br>
+     * <b>null</b> if throws NumberFormatException <br>
+     * <b>instance</b> of Double with your parameters, otherwise
+     * @throws NumberFormatException if {@code src} does not contain a valid string representation of a double.
+     */
     public static Double parseToDouble(String src) {
         if (src == null) {
             return null;
@@ -51,6 +78,15 @@ public final class ParserUtils {
         return null;
     }
 
+    /**
+     * Parsing String to Float
+     *
+     * @param src the float value represented by the string argument.
+     * @return <b>null</b> if {@code src} == null <br>
+     * <b>null</b> if throws NumberFormatException <br>
+     * <b>instance</b> of Float with your parameters, otherwise
+     * @throws NumberFormatException if {@code src} does not contain a valid string representation of a float.
+     */
     public static Float parseToFloat(String src) {
         if (src == null) {
             return null;
@@ -63,6 +99,15 @@ public final class ParserUtils {
         return null;
     }
 
+    /**
+     * Parsing String to Integer
+     *
+     * @param src string containing the int representation to be parsed
+     * @return <b>null</b> if {@code src} == null <br>
+     * <b>null</b> if throws NumberFormatException <br>
+     * <b>instance</b> of Integer with your parameters, otherwise
+     * @throws NumberFormatException if {@code src} does not contain a valid string representation of a integer.
+     */
     public static Integer parseToInteger(String src) {
         if (src == null) {
             return null;
@@ -75,6 +120,13 @@ public final class ParserUtils {
         return null;
     }
 
+    /**
+     * Parsing String to Uri
+     *
+     * @param src string with Uri
+     * @return <b>null</b> if {@code src} == null <br>
+     * <b>Uri</b> for this given uri string, otherwise
+     */
     public static Uri parseToUri(String src) {
         if (src == null) {
             return null;
