7ce6425ce68da71f5f05812dcfad86a7c7e42c1e
magora_vlasov <vlasov@magora-systems.com>
2016-07-04 15:21:52 +0600

    Add doc for CommonInteractor

--- common-module/src/main/java/com/magorasystems/mafmodules/common/mvp/interactor/CommonInteractor.java
+++ common-module/src/main/java/com/magorasystems/mafmodules/common/mvp/interactor/CommonInteractor.java
@@ -16,10 +16,23 @@ public abstract class CommonInteractor<R> implements BaseInteractor<R> {
     private final CompositeSubscription subscription = new CompositeSubscription();
     private final SchedulersUtils.CoreScheduler coreScheduler;
 
-    protected CommonInteractor( SchedulersUtils.CoreScheduler scheduler) {
+    /**
+     * Set your scheduler with background and main thread
+     *
+     * @param scheduler scheduler
+     */
+    protected CommonInteractor(SchedulersUtils.CoreScheduler scheduler) {
         this.coreScheduler = scheduler;
     }
 
+    /**
+     * Use this for execute your task with schedulers. <br>
+     * Add to instance of {@link CompositeSubscription} your {@code observer} extends R <br>
+     * with schedulers from {@link com.magorasystems.mafmodules.common.utils.SchedulersUtils.CoreScheduler}
+     *
+     * @param observer   your observer
+     * @param subscriber your subscriber
+     */
     @Override
     public void execute(Observable<? extends R> observer, Subscriber<R> subscriber) {
         subscription.add(observer
@@ -27,6 +40,12 @@ public abstract class CommonInteractor<R> implements BaseInteractor<R> {
                 .subscribe(subscriber));
     }
 
+    /**
+     * Unsubscribe all subscribers with CompositeSubscription <br>
+     * so that the CompositeSubscription is empty and able to manage new subscriptions.
+     *
+     * @see CompositeSubscription
+     */
     @Override
     public void unsubscribe() {
         subscription.clear();
