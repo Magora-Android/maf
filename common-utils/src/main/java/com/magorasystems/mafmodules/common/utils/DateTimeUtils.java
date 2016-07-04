e6ece7fbefda8a464f65ef5f6c6b6116202b9c55
magora_vlasov <vlasov@magora-systems.com>
2016-07-04 11:56:01 +0600

    Add doc for DateTimeUtils

--- common-utils/src/main/java/com/magorasystems/mafmodules/common/utils/DateTimeUtils.java
+++ common-utils/src/main/java/com/magorasystems/mafmodules/common/utils/DateTimeUtils.java
@@ -20,6 +20,18 @@ public final class DateTimeUtils {
 
     }
 
+    /**
+     * Parsing date from string with default Locale on device (JVM)
+     *
+     * @param src    string for parsing
+     * @param format the format (pattern) describing the date and time format <br> <br>
+     *               "yyyy.MM.dd" (uppercase of MM is important) <br>
+     *               "hh:mm a" returned your time, but date is default = 01.01.1970 <br>
+     *               for more examples see:  http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+     * @return instance of Date with parsed parameters or <b>null</b> if throws exception
+     * @throws ParseException       if the beginning of the {@code src} cannot be parsed.
+     * @throws NullPointerException if the given {@code format} is null
+     */
     public static Date parseTimeString(final String src, final String format) {
         try {
             DateFormat formatter = new SimpleDateFormat(format, Locale.getDefault());
@@ -30,6 +42,16 @@ public final class DateTimeUtils {
         return null;
     }
 
+    /**
+     * @param date   your Date with time
+     * @param format the format (pattern) describing the returned date and/or time format <br> <br>
+     *               "yyyy.MM.dd" (uppercase of MM is important) <br>
+     *               "hh:mm a" returned your time, but date is default = 01.01.1970 <br>
+     *               for more examples see:  http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+     * @return the formatted time string or <b>null</b> if throws exception
+     * @throws ParseException       if the beginning of the {@code src} cannot be parsed.
+     * @throws NullPointerException if the given {@code format} is null
+     */
     public static String formatTime(final Date date, final String format) {
         try {
             DateFormat formatter = new SimpleDateFormat(format, Locale.getDefault());
@@ -40,10 +62,23 @@ public final class DateTimeUtils {
         return null;
     }
 
+    /**
+     * @param start instance of Date with start date
+     * @param end   instance of Date with end date
+     * @return <b>true</b> if time between start and end, <b>false</b> otherwise
+     */
     public static boolean isTimeBetween(Date start, Date end) {
         return isTimeBetween(new Date(), start, end);
     }
 
+    /**
+     * @param current current Date
+     * @param start   start Date
+     * @param end     end Date
+     * @return <b>false</b> if {@code start} after {@code end} <br>
+     * <b>false</b> if {@code start} or {@code end} is null <br>
+     * <b>correct answer</b> otherwise
+     */
     public static boolean isTimeBetween(Date current, Date start, Date end) {
         if (start == null || end == null) {
             return false;
